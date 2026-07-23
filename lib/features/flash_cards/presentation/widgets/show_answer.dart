7d199f589aa193dbb5dafb7b262067c4d1077925
SA92005 <sa9432216@gmail.com>
2026-07-23 11:15:57 +0300

    make previes and next buttons

--- lib/features/flash_cards/presentation/widgets/show_answer.dart
+++ lib/features/flash_cards/presentation/widgets/show_answer.dart
@@ -1,21 +1,29 @@
 import 'package:flashcard_quiz_app/core/theme/app_colors.dart';
 import 'package:flutter/material.dart';
 
-class ShowAnswer extends StatelessWidget {
-  const ShowAnswer({super.key, required this.onPressed});
-  final void Function()? onPressed;
+class CommonButton extends StatelessWidget {
+  const CommonButton({
+    super.key,
+    required this.onPressed,
+    required this.text,
+    this.width = double.infinity,
+  });
+
+  final VoidCallback? onPressed;
+  final String text;
+  final double width;
+
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      width: double.infinity,
+      width: width,
       child: ElevatedButton(
         style: ElevatedButton.styleFrom(
           backgroundColor: AppColors.primary,
           foregroundColor: AppColors.card,
         ),
         onPressed: onPressed,
-
-        child: const Text("Show Answer"),
+        child: Text(text),
       ),
     );
   }
